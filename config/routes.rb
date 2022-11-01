2af4b316745d3aa8b8bb144b37b8eb4367d9c36f
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-10-31 22:50:23 -0500

    authentication logic and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/home" => "pages#home"
   get "/about" => "pages#about"
+
+  # sign up
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
+  # sign in
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
+  
+  # log out
+  delete 'logout', to: 'sessions#destroy'
+  
+  # password
+  get 'password', to: 'passwords#edit', as: 'edit_password'
+  patch 'password', to: 'passwords#edit', as: 'edit_password'
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
 end
