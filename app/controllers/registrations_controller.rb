cdf8c0621cba62f0694044cc52571da1ba5350ad
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-11-01 21:25:19 -0500

    fix syntax error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < ApplicationController
 			session[:user_id] = @user.id
 			redirect_to root_path, notice: 'Successfully created account'
 		else
-			render: new
+			render :new
 		end
 	end
 	private
