fe52f12e853117489c9bd90a4476dfe3b2fd5a56
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-11-07 17:09:17 -0600

    issues controller and views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,16 @@
 class IssuesController < ApplicationController
+	def new
+	end
+	def search; end
+	def get_results
+		url = "https://comicvine.gamespot.com/api/search/?format=json&api_key=#{ENV['COMICVINE_KEY']}&query=%22Amazing%20Spiderman%20issue%2036%22&resources=issue"
+		response = RestClient.get(url)
+		responseObj = JSON.parse(response.body)
+		@result = responseObj['results'][0]
+	end
+	def view; end
+	def edit; end
+	def search_params
+		params.permit(:search)
+	end
 end
